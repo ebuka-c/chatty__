3ff30d7198b8a2eaecdab79367b7764fcb7bfcdc
ebuka-c <chiebuka873@gmail.com>
2023-08-12 15:32:40 +0100

    add firebase core, set default options to current platform and sign in with google

--- lib/global.dart
+++ lib/global.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'common/store/storage.dart';
 import 'common/store/user.dart';
 
@@ -9,7 +10,9 @@ class Global {
     //since it's async, app will wait until everything is loaded
     WidgetsFlutterBinding
         .ensureInitialized(); //making sure dependencies are loaded before widgets
-    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,)
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
     await Get.putAsync<StorageService>(
         () => StorageService().init()); //initialize storage service
     Get.put<UserStore>(UserStore());
